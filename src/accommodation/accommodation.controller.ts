d354cbc28d1facf81f5a149ae6cd973b508dbea2
Agnes4Him <ojuhagnes@gmail.com>
2023-01-16 21:24:19 +0100

    Added status code 200 to successful responses

--- src/accommodation/accommodation.controller.ts
+++ src/accommodation/accommodation.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post, Get } from '@nestjs/common';
-import { Body } from '@nestjs/common/decorators';
+import { Controller, Post, Get, HttpStatus } from '@nestjs/common';
+import { Body, HttpCode } from '@nestjs/common/decorators';
 import { AccommodationService } from './accommodation.service';
 
 @Controller('accommodation')
 export class AccommodationController {
     constructor(private accommodationService: AccommodationService) {}
+    @HttpCode(HttpStatus.OK)
     @Post('add')
     add(@Body() dto: any) {
         return this.accommodationService.add(dto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Get('search') 
     search(@Body() dto: any) {
         return this.accommodationService.search(dto)
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,26 +1,29 @@
-import { Controller, Post, Put, Get, Delete } from '@nestjs/common';
-import { Body } from '@nestjs/common/decorators';
+import { Controller, Post, Put, Get, Delete, HttpStatus } from '@nestjs/common';
+import { Body, HttpCode } from '@nestjs/common/decorators';
 import { StudentService } from './student.service';
 
 @Controller('student')
 export class StudentController {
     constructor(private studentService: StudentService) {}
-
+    @HttpCode(HttpStatus.OK)
     @Post('register')
     register(@Body() dto: any) {
         return this.studentService.register(dto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Put('update')
     update(@Body() dto: any) {
         return this.studentService.update(dto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Delete('remove')
     remove(@Body() dto: any) {
         return this.studentService.remove(dto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Get('list')
     list() {
         return this.studentService.list()
